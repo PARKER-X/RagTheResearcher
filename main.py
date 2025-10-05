1b2f52f8d89e443c5d40b431b6942359ce285182
Harsh Rawat <harshrawat096@gmail.com>
2025-10-05 12:19:51 +0530

    inngest setup!!

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from ragtheresearcher!")
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+from inngest.experimental import ai
+from dotenv import load_dotenv
+import uuid
+import os
+import datetime
+
+# env 
+load_dotenv()
+
+# Ingest client
+inngest_client = inngest.Inngest(
+    app_id="rag_the_researcher",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
 
 
-if __name__ == "__main__":
-    main()
+# Create an Inngest function
+@inngest_client.create_function(
+    fn_id="Rag: Ingest Pdf",
+    # Event that triggers this function
+    trigger=inngest.TriggerEvent(event="rag/ingest_pdf"),
+)
+async def rag_ingest_pdf(ctx: inngest.Context) -> str:
+    ctx.logger.info(ctx.event)
+    return "done"
+
+
+app=FastAPI()
+
+# Serve the Inngest endpoint
+inngest.fast_api.serve(app, inngest_client, [])
+
